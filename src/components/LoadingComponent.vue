5fe1f954800a5759081346dc961bb119596ea0c9
nazariozaccagnino <nazario.zaccagnino@gmail.com>
2024-04-15 18:25:47 +0200

    loading rule completed  with some style

--- src/components/LoadingComponent.vue
+++ src/components/LoadingComponent.vue
@@ -1,15 +1,23 @@
 <template>
-    <div>
-        loading
+    <div class="d-flex align-items-center justify-content-center p-5">
+        <div>
+            <h2>Caricamento</h2>
+        </div>
+        <div>
+            <img src="https://i.gifer.com/ZKZg.gif" alt="Loader img">
+        </div>
     </div>
+
 </template>
 
 <script>
-    export default {
-        name: 'LoadingComponent'
-    }
+export default {
+    name: 'LoadingComponent'
+}
 </script>
 
 <style lang="scss" scoped>
-
+img {
+    max-width: 50px;
+}
 </style>
