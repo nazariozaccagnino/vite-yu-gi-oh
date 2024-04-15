e2b0422e540e3bb7717a0f960da06e7eeacfce89
nazariozaccagnino <157475976+nazariozaccagnino@users.noreply.github.com>
2024-04-15 15:25:59 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <i class="fa fa-solid fa-home"></i>
+    <h1 class="text-center">Test</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    data(){
+      return{
+
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
