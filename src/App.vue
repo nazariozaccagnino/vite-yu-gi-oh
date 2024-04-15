da5d15c5d6412b81fcd97199fc95b5449a144c09
nazariozaccagnino <nazario.zaccagnino@gmail.com>
2024-04-15 15:37:19 +0200

    created the data.js file

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
   <div>
     <i class="fa fa-solid fa-home"></i>
-    <h1 class="text-center">Test</h1>
+    <h1 class="text-center">{{ store.prova }}</h1>
   </div>
 </template>
 
 <script>
+import { store } from './data/store.js';
   export default {
     name: 'App',
     data(){
       return{
-
+        store
       }
     }
   }
