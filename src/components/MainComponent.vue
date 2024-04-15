7e667d9cc04d68a92bbef9ab014e17305e71850e
nazariozaccagnino <nazario.zaccagnino@gmail.com>
2024-04-15 18:17:03 +0200

    created the loadingpage component and initial error handling

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -14,16 +14,15 @@
             </div>
         </div>
         <div class="my-4">
-            <!-- <ul>
-                <li v-for="(item, index) in store.cardArray">
-                    {{item.name}}
-                </li>
-            </ul> -->
-            <div class="row">
+           <div class="text-center">
+            <h4>Card caricate: {{ this.store.cardArray.length }}</h4>
+           </div>
+            <div class="row" v-if="!store.loading">
                 <div class="col-3 d-flex align-items-stretch p-2"  v-for="(item, index) in store.cardArray">
                     <div class="card p-2">
                         <img :src="`${this.store.imageUrl}${item.id}.jpg`" class="card-img-top" :alt="item.name">
                         <h5 class="card-title text-center">{{item.name}}</h5>
+                        <h6 class="subtitle">Tipo: {{ item.type }}</h6>
                         <div class="card-body">                            
                             <p class="card-text">{{ item.desc }}</p>
                             <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
@@ -31,15 +30,20 @@
                     </div>
                 </div>
             </div>
+                <LoadingComponent v-else/>
         </div>
     </div>
 </template>
 
 <script>
 import { store } from '../data/store.js';
+import LoadingComponent from './LoadingComponent.vue'
 import axios from 'axios'
 export default {
     name: 'MainComponent',
+    components:{
+        LoadingComponent
+    },
 
     data() {
         return {
@@ -48,10 +52,18 @@ export default {
     },
     methods:{
         getCards(){
+            this.store.loading = true;
             axios.get(store.apiUrl).then((elements) => {
-            this.store.cardArray = elements.data.data
-            console.log(this.store.cardArray);
-        })
+                this.store.cardArray = elements.data.data
+                console.log(this.store.cardArray);
+                })
+                .catch((error)=> {
+                    // this.store.cardArray = elements.data.error
+                    console.log(error);
+                })
+                .finally(()=> {
+                this.store.loading = false;
+                });
         }
     },
     created() {
@@ -61,4 +73,11 @@ export default {
 
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.subtitle{
+  text-align: center;
+  font-size: 1rem;
+  color: grey; 
+}
+
+</style>
--- src/data/store.js
+++ src/data/store.js
@@ -1,9 +1,10 @@
 import { reactive } from "vue";
 
 export const store = reactive({
-    apiUrl: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?language=it&num=20&offset=0',
+    apiUrl: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?language=it&num=20&offset=0&sort=new',
+    // apiUrl:'https://db.ygoprodeck.com/api/v7/cardinfo.php?type=Effect%20Monster&attribute=wood&num=2', error 400 test
     imageUrl: 'https://images.ygoprodeck.com/images/cards_small/',
     cardArray: [],
-
+    loading: false,
 
 });
